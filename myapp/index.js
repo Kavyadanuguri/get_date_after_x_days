d1140754d8dcaf009d68b469b6f42422d2db2c63
root <you@example.com>
2023-11-06 07:34:30 +0000

    get date after x days solution

--- myapp/index.js
+++ myapp/index.js
@@ -1,7 +1,8 @@
 const addDays = require("date-fns/addDays");
 
 const kavya = (days) => {
-  const date = addDays(new Date(2021, 0, 11), days);
-  return `${date.getDate()}- ${date.getmMonth() + 1}-${date.getFullYear()}`;
+  const date = addDays(new Date(2020, 7, 22), days);
+  return `${date.getDate()} - ${date.getMonth()} - ${date.getFullYear()}`;
 };
+console.log(kavya(10));
 module.exports = kavya;
